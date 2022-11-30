e7e19ad8dd63f0b1680b9196f6c5d91ce07af776
joão pedro fernandes teixeira <pedro.fernandes1058@gmail.com>
2022-11-29 23:43:22 -0300

    adiciando script nos botoes tip

--- script/script.js
+++ script/script.js
@@ -1,85 +1,42 @@
 
 //variables
-const bill = document.querySelector("#numberbill")
-const people = document.querySelector("#numberpeople")
-const percent = document.querySelectorAll(".percent_number")
-const custom = document.querySelector(".percent_number_custom")
+const bill = document.getElementById("numberbill");
 const error = document.querySelectorAll(".error")
-const valor = document.querySelectorAll(".value")
-const v = document.querySelectorAll(".v")
+const tip = document.querySelectorAll(".tip")
 
-//Functions
-function number(value){
-    return !isNaN(value)
+
+bill.addEventListener("input", billvalue);
+
+let billValor = 0.0; // Valor Padrão
+
+
+//functions
+
+function validate(value) {
+    return isNaN(value)
 }
 
-function calculate() {
-    return bill * people
-}
+function billvalue() {
+    billvalue = parseFloat(bill.value)
+    console.log(billvalue)
 
-bill.addEventListener("change", () => {
-    if(number(bill.value.split(" ").join(""))) {
-        bill.style.border = "0.17rem green solid"
-        error[0].textContent = ""
-    } else {
-        bill.style.border = "0.17rem red solid"
-        bill.style.placeholder = "00.00"
-        error[0].textContent = "Number only"
-        error[0].style.color = "red"
-    } 
-})
-
-people.addEventListener("change", () => {
-
-    if(number(people.value.split(" ").join(""))) {
-        people.style.border = "0.17rem green solid"
-        error[1].textContent = ""
-    } else {
-        people.style.border = "0.17rem red solid"
-        error[1].textContent = "Number only"
-        error[1].style.color = "red"
+    if (bill.value.includes(',')) {
+        bill.value = bill.value.replace(',' , '.')
     }
+    if(validate(bill.value.split(" ").join(""))) {
+        bill.style.border = "0.175rem red solid"
+        error[0].textContent = "Number only, please"
+        error[0].style.color = "red"
+    } else {
+        bill.style.border = "0.175rem green solid"
+        error[0].textContent = ""
+    }
+}
+
+console.log(bill)
+
+tip.forEach(btn => {
+    btn.addEventListener("click", setColor)
 })
 
 
-percent[0].addEventListener("click", () => {
-    percent[0].style.backgroundColor = "hsl(172, 67%, 45%"
-    percent[1].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[2].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[3].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[4].style.backgroundColor = "hsl(183, 100%, 15%)"
-})
-
-percent[1].addEventListener("click", () => {
-    percent[1].style.backgroundColor = "hsl(172, 67%, 45%"
-    percent[0].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[2].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[3].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[4].style.backgroundColor = "hsl(183, 100%, 15%)"
-})
-
-percent[2].addEventListener("click", () => {
-    percent[2].style.backgroundColor = "hsl(172, 67%, 45%"
-    percent[1].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[0].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[3].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[4].style.backgroundColor = "hsl(183, 100%, 15%)"
-})
-
-percent[3].addEventListener("click", () => {
-    percent[3].style.backgroundColor = "hsl(172, 67%, 45%"
-    percent[1].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[2].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[0].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[4].style.backgroundColor = "hsl(183, 100%, 15%)"
-})
-
-percent[4].addEventListener("click", () => {
-    percent[4].style.backgroundColor = "hsl(172, 67%, 45%"
-    percent[1].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[2].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[3].style.backgroundColor = "hsl(183, 100%, 15%)"
-    percent[0].style.backgroundColor = "hsl(183, 100%, 15%)"
-})
-
-valor[0].textContent = number(calculate())
